da5eb13bb486964d8e10221b499a49a264e40714
kevin kachhadiya <kachhadiyakevin03@gmail.com>
2025-04-12 18:55:58 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace allocation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
